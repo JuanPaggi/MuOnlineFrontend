455046dd31da8d6cb846b0ce2a4d503a97c81cba
Joaquin <reeninja1996@gmail.com>
2020-04-12 17:59:49 -0300

    Paginado ranking y Guild rank

--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -12,11 +12,18 @@ export class RankingComponent implements OnInit {
   ranking_show: PersonajeDatosDto[];
   ranking_fil: PersonajeDatosDto[];
 
+  public btn_siguiente: boolean;
+  public btn_anterior: boolean;
+  private show_btn: number;
+
+  private pagina: number;
+
   constructor(private pj_service: PersonajesService) {
     this.ranking_show = [];
   }
 
   ngOnInit(): void {
+    this.pagina = 0;
     this.load_ranking();
   }
 
@@ -25,7 +32,7 @@ export class RankingComponent implements OnInit {
       .get_character(new PersonajeDatosDto())
       .subscribe((response) => {
         this.ranking = response;
-        this.ranking_show = this.ranking;
+        this.filtrar_pagina(0);
       });
   }
 
@@ -39,7 +46,62 @@ export class RankingComponent implements OnInit {
     this.ranking_show = this.ranking_fil;
   }
 
+  private button_control(num: number) {
+    switch (num) {
+      case 0:
+        this.btn_anterior = false;
+        this.btn_siguiente = true;
+        break;
+      case 1:
+        this.btn_anterior = true;
+        this.btn_siguiente = false;
+        break;
+      case 2:
+        this.btn_anterior = true;
+        this.btn_siguiente = true;
+    }
+  }
+
+  private paginado(inicio: number, fin: number) {
+    this.ranking_fil = [];
+    for (let i = inicio; i < fin; i++) {
+      if (this.ranking[i]) {
+        this.ranking_fil.push(this.ranking[i]);
+      } else {
+        this.show_btn = 1;
+        break;
+      }
+    }
+    this.ranking_show = this.ranking_fil;
+  }
+
+  public filtrar_pagina(boton: number) {
+    switch (boton) {
+      case 0:
+        this.pagina = 0;
+        this.show_btn = 0;
+        break;
+      case 1:
+        if (this.pagina >= 20) {
+          this.pagina -= 20;
+        }
+        this.show_btn = 0;
+        break;
+      case 2:
+        if (this.pagina <= 80) {
+          this.pagina += 20;
+        }
+        this.show_btn = 1;
+        break;
+    }
+    if (this.pagina != 0 && this.pagina != 80) {
+      this.show_btn = 2;
+    }
+    this.paginado(this.pagina, this.pagina + 20);
+    this.button_control(this.show_btn);
+  }
+
   public reset_filter() {
-    this.ranking_show = this.ranking;
+    this.filtrar_pagina(0);
   }
 }
